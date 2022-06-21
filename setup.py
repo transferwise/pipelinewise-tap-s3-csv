e6f77ae81c1c2590188635f7381a0a376a5236aa
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2022-06-21 03:54:47 +0000

    Temporary fix to use forked singer-encodings

--- setup.py
+++ setup.py
@@ -19,8 +19,10 @@ setup(name='pipelinewise-tap-s3-csv',
       py_modules=['tap_s3_csv'],
       install_requires=[
           'boto3==1.23.10',
-          'singer-encodings==0.1.*',
           'pipelinewise-singer-python==2.0.*',
+          'singer-encodings==0.1.*',
+          # Public repository
+          'singer-encodings @ git+https://github.com/s7clarke10/singer-encodings@feature/remove_singer_dependency',
           'voluptuous==0.13.1',
           'ujson==5.2.0',
           'messytables==0.15.*',
@@ -34,7 +36,10 @@ setup(name='pipelinewise-tap-s3-csv',
               'pytest==7.1.*',
               'pylint==2.12.*',
               'pytest-cov==3.0.*'
-          ]
+          ],
+          'postinstall': [
+              'singer-encodings==0.1.*',
+          ],
       },
       entry_points='''
           [console_scripts]
