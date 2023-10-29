e5daa7f2e2d8f7c4add210210249d97e948bd379
P.A. Masse <pa.masse@split.io>
2023-10-29 16:25:55 +0000

    match transferwise

--- tap_s3_csv/s3.py
+++ tap_s3_csv/s3.py
@@ -162,19 +162,7 @@ def sample_file(config, table_spec, s3_path, sample_rate):
     """
     file_stream = get_file_stream(config, s3_path)
 
-    # csv.get_row_iterator will check key-properties exist in the csv
-    # so we need to give them the list minus the meta field such as SDC_SOURCE_FILE_COLUMN or others
-    reduced_table_spec = {}
-    reduced_table_spec["key_properties"] = table_spec.get("key_properties", []).copy()
-    if SDC_SOURCE_BUCKET_COLUMN in reduced_table_spec["key_properties"]:
-        reduced_table_spec["key_properties"].remove(SDC_SOURCE_BUCKET_COLUMN)
-    if SDC_SOURCE_FILE_COLUMN in reduced_table_spec["key_properties"]: 
-        reduced_table_spec["key_properties"].remove(SDC_SOURCE_FILE_COLUMN)
-    if SDC_SOURCE_LINENO_COLUMN in reduced_table_spec["key_properties"]: 
-        reduced_table_spec["key_properties"].remove(SDC_SOURCE_LINENO_COLUMN)
-
-
-    iterator = get_row_iterator(file_stream, reduced_table_spec) #pylint:disable=protected-access
+    iterator = get_row_iterator(file_stream, table_spec) #pylint:disable=protected-access
 
     current_row = 0
 
